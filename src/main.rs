8b9e2ed2ac166b17c6d75a6f38b312fba3ab453c
simlf <simon.le-fourn@epitech.eu>
2022-06-28 05:20:55 +0200

    :recycle: Modules for more clarity in the filetree

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 // Extern crate
 use clap::Parser;
 use anyhow::Result;
+use std::{io::Write, collections::VecDeque};
 
 // Local
 pub mod pizza;
@@ -22,7 +23,10 @@ fn main() -> Result<()> {
     if args.cooking_time < 0.0 || args.cooking_time > 1.0 {
         panic!("Cooking time should be between 0 and 1.");
     }
-    Shell::run();
+    let mut shell : Shell = Shell {
+        pizzas: VecDeque::new()
+    };
+    shell.run();
     Ok(())
 }
 
