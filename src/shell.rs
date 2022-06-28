07c1f89dcb0c274907e3e7a080887e941791891d
simlf <simon.le-fourn@epitech.eu>
2022-06-28 05:20:12 +0200

    :sparkles: Parser handling multiples pizzas orders

--- src/shell.rs
+++ src/shell.rs
@@ -1,25 +1,17 @@
+use core::num;
 // Extern crate
-use std::io::Write;
+use std::{io::Write, collections::VecDeque};
 
 // Local crate
 use crate::pizza::Pizza;
 
-pub struct Shell;
+// regina XL x3 ; margarita S x6 ; americana M x2 ; fantasia L x1
+pub struct Shell {
+    pub pizzas: VecDeque<Pizza>
+}
 
 impl Shell {
-    pub fn run() {
-        loop {
-            let input = Self::prompt("> ");
-            match input.as_str() {
-                "status" => println!("Status"),
-                _ => {
-                    Self::check_orders(input);
-                }
-            }
-        }
-    }
-
-    fn prompt(name:&str) -> String {
+    fn prompt(&self, name:&str) -> String {
         let mut line = String::new();
 
         print!("{}", name);
@@ -29,13 +21,47 @@ impl Shell {
         line.trim().to_string()
     }
 
-    fn check_orders(input: String){
-        if input.contains(";") {
-            let command: Vec<&str> = input.split(" ;").collect();
-            println!("{:?}", command);
-        } else {
-            let command: Vec<&str> = input.split_whitespace().collect();
-            let pizza: Pizza = Pizza::parse(&input).unwrap();
+    pub fn run(&mut self) {
+        loop {
+            let input = self.prompt("> ");
+            match input.as_str() {
+                "status" => println!("Status"),
+                _ => {
+                    self.check_orders(input);
+                }
+            }
         }
     }
+
+    fn check_orders(&mut self, input: String) {
+        if input.contains(";") {
+            self.order_pizzas(input);
+        } else {
+            self.order_pizza(input);
+        }
+    }
+
+    fn order_pizzas(&mut self, input: String) {
+        let commands: Vec<&str> = input.split(" ; ").collect();
+        for command in commands {
+                self.order_pizza(command.to_string());
+        }
+
+    }
+
+    fn order_pizza(&mut self, command: String) {
+        let mut pizza_number = self.check_pizza_number(command.to_string());
+        while pizza_number >= 1 {
+            let pizza: Pizza = Pizza::parse(&command).unwrap();
+            self.pizzas.push_back(pizza);
+            pizza_number -= 1;
+        }
+    }
+
+    fn check_pizza_number(&self, number: String) -> usize {
+        let iter: String = number.split_whitespace().last().unwrap().split("x").collect();
+
+        iter.parse().unwrap()
+    }
 }
+
